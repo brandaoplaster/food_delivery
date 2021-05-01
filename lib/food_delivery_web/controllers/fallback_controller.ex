90a86e472cc827a984a4d44af20a8ba4cb89a3e6
brandaoplaster <brandaoplaster@gmail.com>
2021-04-30 23:25:45 -0300

    feat: Adding the possibility to change error status

--- lib/food_delivery_web/controllers/fallback_controller.ex
+++ lib/food_delivery_web/controllers/fallback_controller.ex
@@ -3,9 +3,9 @@ defmodule FoodDeliveryWeb.FallbackController do
 
   alias FoodDeliveryWeb.ErrorView
 
-  def call(conn, {:error, result}) do
+  def call(conn, {:error, %{status: status, result: result}}) do
     conn
-    |> put_status(:bad_request)
+    |> put_status(status)
     |> put_view(ErrorView)
     |> render("400.json", result: result)
   end
