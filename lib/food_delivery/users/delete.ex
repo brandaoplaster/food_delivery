5c78a2a259ddccc8e095f2c7d615d327ae0e6152
brandaoplaster <brandaoplaster@gmail.com>
2021-06-01 13:28:57 -0300

    refactor: Removing uuid lookup from user context

--- lib/food_delivery/users/delete.ex
+++ lib/food_delivery/users/delete.ex
@@ -1,13 +1,10 @@
 defmodule FoodDelivery.Users.Delete do
-  alias Ecto.UUID
   alias FoodDelivery.{Error, Repo, User}
 
   def call(id) do
-    with {:ok, uuid} <- UUID.cast(id),
-         %User{} = user <- Repo.get(User, uuid) do
+    with %User{} = user <- Repo.get(User, id) do
       Repo.delete(user)
     else
-      :error -> {:error, Error.build_id_format_error()}
       nil -> {:error, Error.build_user_not_found_error()}
     end
   end
--- lib/food_delivery/users/get.ex
+++ lib/food_delivery/users/get.ex
@@ -1,13 +1,10 @@
 defmodule FoodDelivery.Users.Get do
-  alias Ecto.UUID
   alias FoodDelivery.{Error, Repo, User}
 
   def by_id(id) do
-    with {:ok, uuid} <- UUID.cast(id),
-         %User{} = user <- Repo.get(User, uuid) do
+    with %User{} = user <- Repo.get(User, id) do
       {:ok, user}
     else
-      :error -> {:error, Error.build_id_format_error()}
       nil -> {:error, Error.build_user_not_found_error()}
     end
   end
