1a2af6a0679f1e3017d8f747534a54b550547dc1
brandaoplaster <brandaoplaster@gmail.com>
2021-06-01 10:32:05 -0300

    chore: [WIP] in tests for create user

---
+++ test/food_delivery/users/create_test.exs
@@ -0,0 +1,32 @@
+defmodule FoodDelivery.Users.CreateTest do
+  use FoodDelivery.DataCase, async: true
+
+  import FoodDelivery.Factory
+
+  alias Ecto.Changeset
+  alias FoodDelivery.{Error, User}
+  alias FoodDelivery.Users.Create
+
+  describe "call/1" do
+    test "when all params are valid, returns the user" do
+      result =
+        build(:user_params)
+        |> Create.call()
+
+      assert {:ok, %User{id: _id, age: 29, email: "Ruby@gmail.com"}} = result
+    end
+
+    test "when there are invalid params, returns an error" do
+      response =
+        build(:user_params, %{age: 15})
+        |> Create.call()
+
+
+
+      expected_response = %{age: ["must be greater than or equal to 18"]}
+
+      assert {:error, %Error{status: :bad_request, result: changeset}} = response
+      assert errors_on(changeset) == expected_response
+    end
+  end
+end
