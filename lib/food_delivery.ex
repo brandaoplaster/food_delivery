1bed08606784a68b538b8ff03e5213a5caba1ada
brandaoplaster <brandaoplaster@gmail.com>
2021-06-20 22:27:28 -0300

    feat: Adding  module for item creation

--- lib/food_delivery.ex
+++ lib/food_delivery.ex
@@ -1,4 +1,6 @@
 defmodule FoodDelivery do
+  alias FoodDelivery.Items.Create, as: ItemCreate
+
   alias FoodDelivery.Users.Create, as: UserCreate
   alias FoodDelivery.Users.Delete, as: UserDelete
   alias FoodDelivery.Users.Get, as: UserGet
@@ -8,4 +10,6 @@ defmodule FoodDelivery do
   defdelegate get_user_by_id(id), to: UserGet, as: :by_id
   defdelegate delete_user(id), to: UserDelete, as: :call
   defdelegate update_user(params), to: UserUpdate, as: :call
+
+  defdelegate create_item(params), to: ItemCreate, as: :call
 end
