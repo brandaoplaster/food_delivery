0456f7f5b63909915bcd4d64794610d713225285
brandaoplaster <brandaoplaster@gmail.com>
2021-06-20 22:59:59 -0300

    chore: Adding migrate to order, payment_method and orders_items

---
+++ priv/repo/migrations/20210621014824_create_payment_method.exs
@@ -0,0 +1,10 @@
+defmodule FoodDelivery.Repo.Migrations.CreatePaymentMethod do
+  use Ecto.Migration
+
+  def change do
+    up_query = "CREATE TYPE payment_method AS ENUM ('mony', 'credit_card', 'debit_card')"
+    down_query = "DROP TYPE payment_method"
+
+    execute(up_query, down_query)
+  end
+end
---
+++ priv/repo/migrations/20210621015032_create_orders_table.exs
@@ -0,0 +1,14 @@
+defmodule FoodDelivery.Repo.Migrations.CreateOrdersTable do
+  use Ecto.Migration
+
+  def change do
+    create table :orders do
+      add :address, :string
+      add :commemts, :string
+      add :payment_method, :payment_method
+      add :user_id, references(:users, type: :binary_id)
+
+      timestamps()
+    end
+  end
+end
